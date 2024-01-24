3bdb434d7ea311c64638dbc44c58749e2ff6cd3b
Wollivan <tim.smith2814@gmail.com>
2024-01-24 21:38:57 +0000

    Add args

--- script.js
+++ script.js
@@ -2,10 +2,14 @@
 
 let time = 600;
 
+if (process.argv[2]) {
+  time = process.argv[2] * 60;
+}
+
 setInterval(() => {
   const minutesLeft = Math.floor(time / 60);
   const secondsLeft = time % 60 < 10 ? `0${time % 60}` : time % 60;
-  console.clear();
+  // console.clear();
   console.log(`${minutesLeft}:${secondsLeft}`);
   time--;
 }, 1000);
